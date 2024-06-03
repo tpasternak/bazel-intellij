a62d6b8a9307269bd805824fe4c05970ade3de97
Daniel Brauner <daniel.brauner@jetbrains.com>
2024-06-03 17:29:59 +0200

    Fixed CLion sdkcompat

--- sdkcompat/v242/com/google/idea/sdkcompat/clion/CMakeNotificationProviderWrapper.java
+++ sdkcompat/v242/com/google/idea/sdkcompat/clion/CMakeNotificationProviderWrapper.java
@@ -17,32 +17,20 @@
 package com.google.idea.sdkcompat.clion;
 
 
+import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.cidr.cpp.cmake.workspace.CMakeNotificationProvider;
-
-import java.util.function.Function;
 import javax.annotation.Nullable;
-import javax.swing.*;
+import javax.swing.JComponent;
 
-// #api223
+// #api224 - CMakeNotificationProvider was removed in ths release
 public class CMakeNotificationProviderWrapper {
-    CMakeNotificationProvider value;
-
-    public CMakeNotificationProviderWrapper(){
-        this.value = new CMakeNotificationProvider();
-    }
 
     @Nullable
     public JComponent createNotificationPanel(VirtualFile virtualFile, FileEditor fileEditor, Project project) {
-        Function<? super FileEditor, ? extends JComponent> notificationProducer =
-            this.value.collectNotificationData(project, virtualFile);
-
-        if (notificationProducer != null) {
-            return notificationProducer.apply(fileEditor);
-        }
-
         return null;
     }
+
+    public static <T> void unregisterDelegateExtension(ExtensionPoint<T> extensionPoint) {}
 }
